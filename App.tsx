9f4fceba598846a75f245bc223fc66e56cdad96e
thoulee <thou_lee@outlook.com>
2024-09-16 18:27:59 +0800

    feat: add basic chat func & in-app update - Update .gitignore to ignore .vscode/ and android/ directories - Update resizeMode in app.json to "cover" for splash image - Update foregroundImage and favicon paths in app.json - Enable updates in app.json - Add new dependencies in package.json

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Button, ToastAndroid } from 'react-native'
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [messages, setMessages] = useState([] as IMessage[])
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: require('./assets/icon.png'),
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = [] as IMessage[]) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
+  return (
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <Button
+        title='Check updates'
+        onPress={async () => {
+          const update = await Updates.checkForUpdateAsync()
+          console.debug(JSON.stringify(update, null, 2))
+
+          if (update.isAvailable) {
+            ToastAndroid.show('Update available', ToastAndroid.SHORT)
+
+            await Updates.fetchUpdateAsync()
+            await Updates.reloadAsync()
+          }
+        }}
+      />
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+          avatar: require('./assets/icon.png'),
+        }}
+        showUserAvatar
+        onLongPressAvatar={(user) => {
+          console.debug(JSON.stringify(user, null, 2))
+        }}
+      />
+    </SafeAreaProvider>
+  )
+}
