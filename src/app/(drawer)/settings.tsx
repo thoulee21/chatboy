982b50b75f74f94928c809305696fac363f330c4
thoulee <thou_lee@outlook.com>
2025-04-06 19:23:49 +0800

    refactor: remove user redirection logic from SettingsScreen

--- src/app/(drawer)/settings.tsx
+++ src/app/(drawer)/settings.tsx
@@ -1,18 +1,11 @@
-import { Redirect, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { List } from "react-native-paper";
 
 import { version } from "@/../package.json";
-import { useUser } from "@/contexts/UserContext";
 
 export default function SettingsScreen() {
-  const user = useUser();
-
-  if (!user?.current) {
-    return <Redirect href="/login" />;
-  }
-
   return (
     <>
       <Stack.Screen options={{ title: "Settings" }} />
